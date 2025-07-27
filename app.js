2c523042855a01090a4f94d25b0bdd4c0bc6cea2
arshia <arshiahasanpor53gmail.com>
2025-07-27 14:00:09 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,137 @@
+ let users = [];
+        let users1 = new Array(4)
+
+        function addUser(name, age, job, ed) {
+            users1[0] = name
+            users1[1] = age
+            users1[2] = job
+            users1[3] = ed
+
+            users.push(users1.join(" "))
+
+        }
+        addUser("mohamad", "23", "teacher", "diploma")
+        addUser("arshia", "55", "teacher", "diploma")
+        console.log(users)
+
+
+
+
+
+       const users = [];
+
+
+
+        function addUser(name, age, job, ed) {
+
+            const profile = name + age + job + ed;
+            users.push(profile)
+
+            console.log(users)
+        }
+        addUser("arshia ", 23, " teacher ", " diploma ")
+
+
+                function student(firstName, GPA,  lesson , class ) {
+
+
+         const student = {
+
+            firstName: "arshia",
+            GPA: 19,
+            class: "ab",
+            lesson: ["math", "Arabic ", "sciences"]
+        }
+
+        console.log(student)
+
+
+        }
+
+        addUser("arshia", 65, "teachr", "diploma");
+
+
+
+        const student = {
+
+            firstName: "arshia",
+            GPA: 19,
+            class: "ab",
+            lesson: ["math", "Arabic ", "sciences"]
+        }
+
+        console.log(student)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                const object1 = {
+            firstName: "software engineer",
+            lastName: "lawyer",
+            mohammed: "technical writer",
+            fullName: function () {
+                return this.firstName + " " + this.lastName;
+            }
+        };
+
+        console.log(object1.fullName());
+
+
+
+        addUser("arshia", 65, "teachr", "diploma");
+
+
+
+        const student = {
+
+            firstName: "arshia",
+            GPA: 19,
+            class: "ab",
+            lesson: ["math", "Arabic ", "sciences"]
+        }
+
+        console.log(student)
+        
+
+
+
+        const person2 = new Object();
+
+
+        person2.firstName = "John";
+        person2.lastName = "Doe";
+        person2.age = 50;
+        person2.eyeColor = "blue";
+
+        console.log(person2)
+
+
+
+
+
+
+
+
+
